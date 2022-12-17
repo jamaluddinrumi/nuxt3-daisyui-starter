bd7ad732a502108b1a92f400c99bfd528151bf45
Jamaluddin Rumi <jamal.rumi@icloud.com>
2022-12-17 17:21:44 +0700

    add inter tight font

--- app.vue
+++ app.vue
@@ -1,3 +1,11 @@
+<script lang="ts" setup>
+import '@fontsource/inter-tight/100.css'
+import '@fontsource/inter-tight/400.css'
+import '@fontsource/inter-tight/400-italic.css'
+import '@fontsource/inter-tight/700.css'
+import '@fontsource/inter-tight/900.css'
+</script>
+
 <template>
   <Head>
     <Title>Nuxt 3 + Daisy UI Starter Template</Title>
@@ -6,3 +14,9 @@
   <NuxtPage />
   <BaseFooter />
 </template>
+
+<style>
+body {
+  font-family: "Inter Tight", sans-serif;
+}
+</style>

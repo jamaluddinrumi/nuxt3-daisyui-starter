10a97b90c555b8f2c513440fefb24da0890a8e4f
Jamaluddin Rumi <jamal.rumi@icloud.com>
2022-12-17 14:04:18 +0700

    add official tailwind plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,11 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+    require("daisyui"),
+  ]
 }

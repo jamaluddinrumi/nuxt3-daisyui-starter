a29f3c57375c1096d7fa5b85e204855cd3ca597e
Jamaluddin Rumi <jamal.rumi@icloud.com>
2022-12-21 20:13:25 +0700

    big fixing
    - use @volar-plugins/eslint instead eslint vscode extension
    - add prettier-plugin-tailwindcss
    - add format script

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
-import { defineNuxtConfig } from 'nuxt/config'
+import { defineNuxtConfig } from "nuxt/config";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss', 'nuxt-icon']
-})
+  modules: ["@nuxtjs/tailwindcss", "nuxt-icon"],
+});

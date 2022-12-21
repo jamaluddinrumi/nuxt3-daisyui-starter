3239c928957bd113b4b95ed4d92a08fc62560434
Jamaluddin Rumi <jamal.rumi@icloud.com>
2022-12-22 06:15:15 +0700

    add @nuxt/image-edge for storyblok

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,20 @@
 import { defineNuxtConfig } from "nuxt/config";
 
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@storyblok/nuxt', "@nuxtjs/tailwindcss", "nuxt-icon"],
-  storyblok: { accessToken: 'ae7No8GPaQ2mzyt2r0rjoQtt' },
+  modules: [
+    "@storyblok/nuxt",
+    "@nuxt/image-edge",
+    "@nuxtjs/tailwindcss",
+    "nuxt-icon",
+  ],
+  storyblok: { accessToken: "ae7No8GPaQ2mzyt2r0rjoQtt" },
+  image: {
+    provider: "storyblok",
+    storyblok: {
+      baseURL: "https://a.storyblok.com",
+      modifiers: {
+        format: "webp",
+      },
+    },
+  },
 });

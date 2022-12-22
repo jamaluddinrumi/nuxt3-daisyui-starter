c9eba3512a0b20d21d37c4643bbe0735a88b3cc0
Jamaluddin Rumi <jamal.rumi@icloud.com>
2022-12-22 23:02:24 +0700

    workaround for tailwind intellisense getting error when include daisyui in tailwind.config.js

---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'tailwindcss'
+
+export default <Config>{
+  content: [
+    "storyblok/**/*.{vue,js}",
+    "components/**/*.{vue,js}",
+    "pages/**/*.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/aspect-ratio"),
+    require("daisyui"),
+  ],
+}
